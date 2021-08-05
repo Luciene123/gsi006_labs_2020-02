acd006f221a1d7668dfac301fc5f82a87a4a7027
Luciene123 <luciene.martins@ufu.br>
2021-08-05 15:22:10 -0300

    alteração

--- Lab09 - Structs/ex01/ex01.c
+++ Lab09 - Structs/ex01/ex01.c
@@ -1,9 +1,24 @@
-#include <stdio.h>
 
-int main(){
-    printf("<<  >>\n");
+#include<stdio.h>
+#include<stdlib.h>
 
-    return 0;
+struct Telefone{
+    char DDD[4];
+    char telefone[15];
+};
+int main()
+{   struct Telefone fone1, fone2, fone3;
+    strcpy(fone1.DDD,"034");
+    strcpy(fone1.telefone,"98888-4444");
+    strcpy(fone2.DDD,"034");
+    strcpy(fone2.telefone,"97777-1414");
+    strcpy(fone3.DDD,"034");
+    strcpy(fone3.telefone,"96666-1212");
+    printf("Telefone1:(%c)/t%c",fone1.DDD,fone1.telefone);
+    printf("Telefone2:(%c)/t%c",fone2.DDD,fone2.telefone);
+    printf("Telefone3:(%c)/t%c",fone3.DDD,fone3.telefone);
+    system("pause");
+return 0;
 }
 
 /*
